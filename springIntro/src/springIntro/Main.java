e96cab09c1f2d287ca538a7c8d3bad328f4bb57d
Dursun <93613110+Dursunipek@users.noreply.github.com>
2022-08-02 18:52:59 +0300

    Spring IoC configuration

--- springIntro/src/springIntro/Main.java
+++ springIntro/src/springIntro/Main.java
@@ -1,5 +1,7 @@
 package springIntro;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -11,12 +13,15 @@ public class Main {
 		// Dependency injection
 		// Yazilim gelistirilebilir, degisebilir olmalidir
 		// Example
-		CustomerManager customerManager = new CustomerManager(new OracleCustomerDal());
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		/*CustomerManager customerManager = new CustomerManager(new OracleCustomerDal());
 		customerManager.Add();
 		CustomerManager customermanager2 = new CustomerManager(new MysqlCustomerDal());
-		customermanager2.Add();
+		customermanager2.Add();*/
+		CustomerManager customerManager = new CustomerManager(context.getBean("database",ICustomerDal.class));
+		customerManager.Add();
 		/*ICustomerDal interface'i kullanarak 2 farkli sinif olusturuldu ve ICustomerDal implement edildi
-		Daha sonrasında CustomerManager sinifi icerisindeki ekleme isleminde olusturulan interface referens edilerek
+		Daha sonrasında CustomerManager sinifi icerisindeki Add() fonksiyonu olusturulan interface referens edilerek
 		o interface'in implement edildigi diger sinifların referans tip ile parametre olarak girilmesine olanak saglandi
 		Bu durumun en buyuk avantajı ilerleyen zamanlarda yeni bir database eklenmek istendiginde impelement edilerek 
 		parametre olarak girilmesi yeterli olacaktir. Kodumuz deigisme ve gelisime acik olacaktir.*/
